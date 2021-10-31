e8451e46d1132cc9ae4a1d78e7cfa371467a2f8b
ngwatso <ngwatson79@yahoo.com>
2021-10-31 15:44:35 -0400

    homepage framed out, no styling or functionality included on this pass, will be done later

--- components/Showcase.js
+++ components/Showcase.js
@@ -2,7 +2,7 @@ import styles from '@/sass/main.module.sass';
 
 export default function Showcase() {
   return (
-    <div>
+    <div className={styles.showcase}>
       {/* Placeholder, h1 will be replaced by MUFI banner, h2 replaced by hero image/video */}
       <h1> PLACEHOLDER -- BANNER </h1>
       <h2>PLACEHOLDER -- HERO VIDEO</h2>
