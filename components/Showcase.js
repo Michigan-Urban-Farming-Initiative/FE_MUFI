d09c5bc9d475dfce704a2b1d9f7dda96d6582f40
ngwatso <ngwatson79@yahoo.com>
2021-10-31 15:01:32 -0400

    added Testimonials component

--- components/Showcase.js
+++ components/Showcase.js
@@ -4,8 +4,8 @@ export default function Showcase() {
   return (
     <div>
       {/* Placeholder, h1 will be replaced by MUFI banner, h2 replaced by hero image/video */}
-      <h1> Welcome to MUFI! </h1>
-      <h2>America&apos;s First Urban Agrihood!</h2>
+      <h1> PLACEHOLDER -- BANNER </h1>
+      <h2>PLACEHOLDER -- HERO VIDEO</h2>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Layout from '@/components/Layout';
 import CallToAction from '@/components/CallToAction';
+import Mission from '@/components/Mission';
+import Testimonials from '@/components/Testimonials';
 import styles from '@/sass/main.module.sass';
 
 export default function HomePage() {
   return (
     <Layout>
       <CallToAction />
-      <h1>Placeholder</h1>
+      <Mission />
+      <Testimonials />
+      <CallToAction />
     </Layout>
   );
 }
