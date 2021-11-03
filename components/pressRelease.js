75fdd25b7240966f47e84b08e09db0293bfb1d85
Dalton Walker <daltonwalkerdw@yahoo.com>
2021-11-03 13:27:22 -0400

    PressRelease component created with logic to pull from back-end. Cards partially created

---
+++ components/pressRelease.js
@@ -0,0 +1,29 @@
+// import { useState, useEffect } from 'react';
+// import pressReleaseCards from './pressReleaseCards';
+
+// const pressRelease = () => {
+//   const [releases, setReleases] = useState({});
+
+//   const fetchPr = async () => {
+//     try {
+//       await res = await axios.get("")
+//       setReleases(res);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchPr();
+//   }, []);
+
+//   return (
+//     <div>
+//       {releases.map((release, id) => {
+//         return <pressRelease key={id} />;
+//       })}
+//     </div>
+//   );
+// };
+
+// export default pressRelease;
---
+++ components/pressReleaseCards.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const pressReleaseCards = () => {
+  return (
+    <div>
+      <img></img>
+      <p>Source</p>
+      <h2>Title of article</h2>
+      <p>Date</p>
+      <p>Author's Name</p>
+    </div>
+  );
+};
+
+export default pressReleaseCards;
